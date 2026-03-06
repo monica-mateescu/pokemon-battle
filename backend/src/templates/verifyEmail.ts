630f73711e2d419f4b3d83c80150d440beee94bc
Monica Mateescu <monica.antonie@gmail.com>
2026-03-06 16:41:25 +0100

    feat: update email verification template to include HTML structure

--- backend/src/templates/verifyEmail.ts
+++ backend/src/templates/verifyEmail.ts
@@ -1,9 +1,18 @@
 export const verifyEmail = ({ name, url }: { name: string; url: string }) =>
-  `<p>Hi ${name},</p>
-    <p>
-      Welcome to Pokémon Battle! Please confirm your email address to complete your registration:
-      <a href="${url}">${url}</a>
-    </p>
-    <p>If you did not sign up for this account, please ignore this email.</p>
-    <p>Best regards,<br/>Pokémon Battle Team</p>
+  `<!DOCTYPE html>
+  <html lang="en">
+    <head>
+      <meta charset="UTF-8" />
+      <title>Verify Your Email</title>
+    </head>
+   <body>
+      <p>Hi ${name},</p>
+      <p>
+        Welcome to Pokémon Battle! Please confirm your email address to complete your registration:
+        <a href="${url}">${url}</a>
+      </p>
+      <p>If you did not sign up for this account, please ignore this email.</p>
+      <p>Best regards,<br/>Pokémon Battle Team</p>
+    </body>
+  </html>
   `;
